a9e2bcbf981613f14dc8c4067e7629f2c4e73d12
hlx <1572938849@qq.com>
2025-02-25 22:37:02 +0800

    实现了分页展示菜品的功能

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,8 +1,12 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface DishService {
     //新增菜品和对应口味
     void saveWishFlavor(DishDTO dishDTO);
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
