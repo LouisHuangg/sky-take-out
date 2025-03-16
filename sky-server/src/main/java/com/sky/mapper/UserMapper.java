3388791eae139b00970e258922066b334a32e48b
hlx <1572938849@qq.com>
2025-03-16 15:30:21 +0800

    实现了用户和营业额统计的功能

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
     @Select("select * from sky_take_out.user where openid = #{openid}")
@@ -13,4 +15,6 @@ public interface UserMapper {
 
     @Select("select * from sky_take_out.user where id = #{userId}")
     User getById(Long userId);
+
+    Integer countUserByMap(Map map);
 }
